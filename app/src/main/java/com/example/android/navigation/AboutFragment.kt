8770bfc4551f465df5d3991bb8365a483ad4d417
Choo Li Jun <choolj-aa16@student.tarc.edu.my>
2020-03-17 22:25:37 +0800

    qwr

--- app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -26,6 +26,7 @@ class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        //qqq
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
